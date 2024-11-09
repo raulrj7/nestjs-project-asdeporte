54589ef64df37fe82757e2ee88ee92cedbaa54a3
Raúl Reyes Jiménez <raul.reyesjimenezico@gmail.com>
2024-11-09 13:51:46 -0600

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';  // Asegúrate de importar esto
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module'; 
 import { TaskModule } from './tasks/task.module'; 
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,  // Esto garantiza que esté accesible globalmente
+      isGlobal: true,
     }),
     AuthModule,
     TaskModule,
--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,16 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-
-  // Habilitar CORS si es necesario
   app.enableCors();
 
-  // Usar validación global
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Elimina propiedades no declaradas
-      forbidNonWhitelisted: true, // Lanza un error si hay propiedades no permitidas
+      whitelist: true,
+      forbidNonWhitelisted: true,
     }),
   );
 
-  const port = process.env.PORT || 3000; // Usar puerto del entorno o por defecto
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   logger.log(`Auth Running On Port ${port}`);
 }
